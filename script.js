3d4e07406516677b7276efb27f9345fce482b76d
Hansi Hirunika <hirunikahansi95@gmail.com>
2026-07-25 19:14:42 +0530

    Add local storage feature

--- script.js
+++ script.js
@@ -1,19 +1,78 @@
-function addTask(){
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+displayTasks();
+
+
+function addTask() {
 
     let input = document.getElementById("taskInput");
 
     let task = input.value;
 
-    if(task===""){
+
+    if(task === ""){
         alert("Please enter a task");
         return;
     }
 
-    let li = document.createElement("li");
 
-    li.textContent = task;
+    tasks.push(task);
 
-    document.getElementById("taskList").appendChild(li);
+    saveTasks();
 
-    input.value="";
+    displayTasks();
+
+    input.value = "";
+
+}
+
+
+
+function displayTasks(){
+
+    let list = document.getElementById("taskList");
+
+    list.innerHTML = "";
+
+
+    tasks.forEach(function(task){
+
+        let li = document.createElement("li");
+
+        li.textContent = task;
+
+
+        let deleteButton = document.createElement("button");
+
+        deleteButton.textContent = "Delete";
+
+
+        deleteButton.onclick = function(){
+
+            tasks = tasks.filter(item => item !== task);
+
+            saveTasks();
+
+            displayTasks();
+
+        };
+
+
+        li.appendChild(deleteButton);
+
+
+        list.appendChild(li);
+
+
+    });
+
+
+}
+
+
+
+function saveTasks(){
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
 }
